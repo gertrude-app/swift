08d7e3015813f8913dcc6f02b5ef883b2a0d9d10
Jared Henderson <jaredh159@users.noreply.github.com>
2023-04-06 16:14:53 -0400

    macapp-rewrite: child features can listen for heartbeats

--- rewrite/App/Sources/App/UserFeature.swift
+++ rewrite/App/Sources/App/UserFeature.swift
@@ -7,9 +7,13 @@ struct UserFeature: Feature {
 
   enum Action: Equatable, Sendable {
     case refreshRules(TaskResult<RefreshRules.Output>)
+    case heartbeat(Heartbeat.Interval)
   }
 
   struct Reducer: FeatureReducer {
+    @Dependency(\.api) var api
+    @Dependency(\.app) var appClient
+
     func reduce(into state: inout State, action: Action) -> Effect<Action> {
       switch action {
 
@@ -18,6 +22,17 @@ struct UserFeature: Feature {
         state.screenshotFrequency = output.screenshotsFrequency
         state.keyloggingEnabled = output.keyloggingEnabled
         state.screenshotsEnabled = output.screenshotsEnabled
+        return .none
+
+      case .heartbeat(let interval) where interval == .everyTwentyMinutes:
+        return .task {
+          await .refreshRules(TaskResult {
+            let appVersion = appClient.installedVersion() ?? "unknown"
+            return try await api.refreshRules(.init(appVersion: appVersion))
+          })
+        }
+
+      case .heartbeat:
         return .none
 
       case .refreshRules(.failure):
