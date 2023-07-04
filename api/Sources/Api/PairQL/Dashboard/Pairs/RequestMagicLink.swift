850dfceed2ace1a29f30642ecc77f097f540ebe5
Jared Henderson <jaredh159@users.noreply.github.com>
2023-07-04 10:45:18 -0400

    api: send no account found email for magic link admin 404

--- api/Sources/Api/PairQL/Dashboard/Pairs/RequestMagicLink.swift
+++ api/Sources/Api/PairQL/Dashboard/Pairs/RequestMagicLink.swift
@@ -1,5 +1,6 @@
 import DuetSQL
 import PairQL
+import Vapor
 
 struct RequestMagicLink: Pair {
   static var auth: ClientAuth = .none
@@ -14,28 +15,40 @@ struct RequestMagicLink: Pair {
 
 extension RequestMagicLink: Resolver {
   static func resolve(with input: Input, in context: Context) async throws -> Output {
+    let email = input.email.lowercased()
+    guard email.isValidEmail else {
+      throw Abort(.badRequest)
+    }
+
     let admin = try? await Current.db.query(Admin.self)
-      .where(.email == .string(input.email.lowercased()))
+      .where(.email == .string(email))
       .first()
 
-    guard let admin = admin else {
-      Current.logger.error("Failed attempt to retrieve magic link for email: `\(input.email)`")
-      #if !DEBUG
-        try? await Task.sleep(seconds: 2)
-      #endif
+    guard let admin else {
+      let noAccountEmail = Email.fromApp(
+        to: email,
+        subject: "Gertrude App Magic Link",
+        html: """
+        A magic login link was requested for this email address, \
+        but no Gertrude account exists with this email address. \
+        Perhaps you signed up with a different email address? <br /><br /> \
+        Or, if you did not request a magic link, you can safely ignore this email.
+        """
+      )
+      try await Current.sendGrid.send(noAccountEmail)
       return .success
     }
 
     let token = await Current.ephemeral.createAdminIdToken(admin.id)
-    let subject = "Gertrude Dashboard Magic Link"
+    let subject = "Gertrude App Magic Link"
     var url = "\(context.dashboardUrl)/otp/\(token.lowercased)"
     if let redirect = input.redirect,
        let encoded = redirect.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
       url += "?redirect=\(encoded)"
     }
     let html = "<a href='\(url)'>Click here</a> to log in to the Gertrude dashboard."
-    let email = Email.fromApp(to: admin.email.rawValue, subject: subject, html: html)
-    try await Current.sendGrid.send(email)
+    let magicLinkEmail = Email.fromApp(to: admin.email.rawValue, subject: subject, html: html)
+    try await Current.sendGrid.send(magicLinkEmail)
 
     return .success
   }
