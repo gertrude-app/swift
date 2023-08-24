4d611901e60751dc7808088be111dd143ec9d044
Jared Henderson <jaredh159@users.noreply.github.com>
2023-08-24 13:26:48 -0400

    api: add missing foreign key on user_devices.device_id

--- api/Sources/Api/Configure/migrations.swift
+++ api/Sources/Api/Configure/migrations.swift
@@ -16,5 +16,6 @@ extension Configure {
     app.migrations.add(DropWaitlistedAdmins())
     app.migrations.add(DeviceRefactor())
     app.migrations.add(AddReleaseNotes())
+    app.migrations.add(DeviceIdForeignKey())
   }
 }
