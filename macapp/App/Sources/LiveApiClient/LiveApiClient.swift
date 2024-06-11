c811798f59633a43116ed1a6cf1241a54546da68
Jared Henderson <jaredh159@users.noreply.github.com>
2024-06-11 11:04:32 -0400

    macapp: wait longer before sending .appLaunched security event

--- macapp/App/Sources/LiveApiClient/LiveApiClient.swift
+++ macapp/App/Sources/LiveApiClient/LiveApiClient.swift
@@ -51,7 +51,11 @@ extension ApiClient: DependencyKey {
     },
     logSecurityEvent: { input in
       // sleep allows us to log events possibly before token/account resolved
-      try? await Task.sleep(nanoseconds: 1_000_000) // 1 millisecond
+      if input.event == "appLaunched" {
+        try? await Task.sleep(nanoseconds: 50_000_000) // 50ms
+      } else {
+        try? await Task.sleep(nanoseconds: 1_000_000) // 1ms
+      }
       guard await accountActive.value else { return }
       guard await userToken.value != nil else { return }
       _ = try? await output(
