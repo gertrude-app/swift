1e5f992a9efacf4c2a3aab8970bdc7f2ecf8fa02
Jared Henderson <jaredh159@users.noreply.github.com>
2024-09-18 11:37:43 -0400

    api: fix email delegation dependency overrides on linux

--- api/Sources/Run/main.swift
+++ api/Sources/Run/main.swift
@@ -15,34 +15,8 @@ try withDependencies { deps in
   deps.stripe = .liveValue
   deps.db = PgClient(threadCount: System.coreCount, env: deps.env)
   deps.aws = .liveValue
-
-  deps.postmark = .live(apiKey: deps.env.postmarkApiKey)
-  deps.sendgrid = .live(apiKey: deps.env.sendgridApiKey)
-  let liveSendgridSend = deps.sendgrid.send
-  let livePostmarkSend = deps.postmark.send
-
-  deps.sendgrid.send = { message in
-    if !isCypressTestAddress(message.firstRecipient.email) {
-      try await liveSendgridSend(message)
-    }
-  }
-
-  if deps.env.mode != .prod {
-    deps.postmark.send = { email in
-      try await liveSendgridSend(.init(postmark: email))
-    }
-  } else {
-    deps.postmark.send = { email in
-      if isCypressTestAddress(email.to) {
-        return
-      } else if isProdSmokeTestAddress(email.to) || isJaredTestAddress(email.to) {
-        try await liveSendgridSend(.init(postmark: email))
-      } else {
-        try await livePostmarkSend(email)
-      }
-    }
-  }
-
+  deps.postmark = .liveValue
+  deps.sendgrid = .liveValue
 } operation: {
   try Configure.app(app)
   try app.run()
