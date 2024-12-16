2df6dd90bbff19e51124f4dd36891abb91e45d5e
Jared Henderson <jaredh159@users.noreply.github.com>
2024-12-16 11:18:08 -0500

    api: handle naming of apps from checkin

---
+++ api/Tests/ApiTests/MacappPairResolvers/CheckInNamedAppTests.swift
@@ -0,0 +1,59 @@
+import DuetSQL
+import Gertie
+import MacAppRoute
+import XCTest
+import XExpect
+
+@testable import Api
+
+final class CheckInNameAppsTests: ApiTestCase {
+  func testNameTotallyUnknownApp() async throws {
+    let app = RunningApp(
+      bundleId: "com.\(UUID().lowercased)",
+      bundleName: "Bundle Name \(UUID())",
+      localizedName: "Localized Name \(UUID())",
+      launchable: true
+    )
+
+    try await self.nameApps([app])
+
+    let retrieved = try await UnidentifiedApp.query()
+      .where(.bundleId == app.bundleId)
+      .first(in: self.db)
+
+    expect(retrieved.localizedName!).toBe(app.localizedName!)
+    expect(retrieved.bundleName!).toBe(app.bundleName!)
+    expect(retrieved.launchable).toEqual(true)
+    expect(retrieved.count).toEqual(1)
+  }
+
+  func testNameUnidentifiedApp() async throws {
+    let app = RunningApp(
+      bundleId: "com.\(UUID().lowercased)",
+      bundleName: "Bundle Name \(UUID())",
+      launchable: false
+    )
+    try await self.db.create(UnidentifiedApp(bundleId: app.bundleId, count: 33))
+
+    try await self.nameApps([app])
+
+    let retrieved = try await UnidentifiedApp.query()
+      .where(.bundleId == app.bundleId)
+      .first(in: self.db)
+
+    expect(retrieved.localizedName).toBeNil()
+    expect(retrieved.bundleName!).toBe(app.bundleName!)
+    expect(retrieved.launchable).toEqual(false)
+    // doesn't increment count, because we send the same apps
+    // over and over in the check-in
+    expect(retrieved.count).toEqual(33)
+  }
+
+  func nameApps(_ namedApps: [RunningApp]) async throws {
+    let user = try await self.userWithDevice()
+    _ = try await CheckIn_v2.resolve(
+      with: .init(appVersion: "1.0.0", filterVersion: "3.3.3", namedApps: namedApps),
+      in: user.context
+    )
+  }
+}
