1f8318908a618c3c886669dbc289954ecf08fc21
Jared Henderson <jaredh159@users.noreply.github.com>
2025-03-10 16:20:19 -0400

    pairql-ios: swift 6 w/ strict concurrency

--- pairql-iosapp/Package.swift
+++ pairql-iosapp/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.10
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
@@ -21,11 +21,7 @@ let package = Package(
         .product(name: "PairQL", package: "pairql"),
         .product(name: "GertieIOS", package: "gertie"),
       ],
-      swiftSettings: [.unsafeFlags([
-        "-Xfrontend", "-warn-concurrency",
-        "-Xfrontend", "-enable-actor-data-race-checks",
-        "-Xfrontend", "-warnings-as-errors",
-      ])]
+      swiftSettings: [.unsafeFlags(["-Xfrontend", "-warnings-as-errors"])]
     ),
   ]
 )
