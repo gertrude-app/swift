04deb80e04c1f9813aaacd1be3c0bae15197d57b
Jared Henderson <jaredh159@users.noreply.github.com>
2025-03-13 10:15:56 -0400

    api: log out block rules input for user troubleshooting

--- api/Sources/Api/PairQL/iOS/BlockRules_v2.swift
+++ api/Sources/Api/PairQL/iOS/BlockRules_v2.swift
@@ -6,7 +6,8 @@ import IOSRoute
 
 extension BlockRules_v2: Resolver {
   static func resolve(with input: Input, in context: Context) async throws -> Output {
-    try await IOSBlockRule.query()
+    with(dependency: \.logger).info("BlockRules_v2: \(input)")
+    return try await IOSBlockRule.query()
       .where(.or(
         .not(.isNull(.group)) .&& .group |!=| input.disabledGroups.map { .string($0.rawValue) },
         .vendorId == (input.vendorId == .init(.zero) ? .init() : input.vendorId)
