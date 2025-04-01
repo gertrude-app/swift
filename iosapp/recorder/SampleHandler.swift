a173c8b13941e11856dd57449683948128b3df0a
Chris Woolfe <chris.woolfe@gmail.com>
2025-04-01 12:28:30 -0400

    demo text extraction on device.

--- iosapp/recorder/SampleHandler.swift
+++ iosapp/recorder/SampleHandler.swift
@@ -10,12 +10,13 @@ import Photos
 import os.log
 import Gertie
 import SensitiveContentAnalysis
+import Vision
 
 class SampleHandler: RPBroadcastSampleHandler {
   var lastSavedDate = Date.now
   var previousScreenThumbnail : CGImage?
   let ciContext = CIContext()
-  let analyzer = SCSensitivityAnalyzer()
+  let sensitivityAnalyzer = SCSensitivityAnalyzer()
   
   override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
     RecordingStatus.didRecordSample()
@@ -45,14 +46,18 @@ class SampleHandler: RPBroadcastSampleHandler {
   }
   
   private func saveScreenToPhotos(_ sampleBuffer: CMSampleBuffer) {
+    os_log("[G•] saveScreenToPhotos")
+    
     guard let currentScreen = getCIImageFrom(sampleBuffer),
           let currentScreenThumbnail = getThumbnailCGImageFrom(currentScreen) else {
       os_log("[G•] Failed to create cgImage.")
       return
     }
+    
     if !currentScreenThumbnail.isNearlyIdenticalTo(previousScreenThumbnail) {
       
-      analyzer.analyzeImage(currentScreenThumbnail) { analysis, error in
+      guard let currentScreenJpegUrl = saveAsJPEG(currentScreen, name:"currentScreen.jpg") else { return }
+      sensitivityAnalyzer.analyzeImage(at: currentScreenJpegUrl) { analysis, error in
         if let error {
           os_log("[G•] Error analyzing image: \(error.localizedDescription)")
         }
@@ -61,16 +66,21 @@ class SampleHandler: RPBroadcastSampleHandler {
         }
       }
       
+      let textAnayzer = VNImageRequestHandler(url: currentScreenJpegUrl)
+      let analysis = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
+      do {
+        try textAnayzer.perform([analysis])
+      } catch {
+        os_log("[G•] Unable to perform the requests: \(error).")
+      }
+      
       previousScreenThumbnail = currentScreenThumbnail.copy()
-      NSLog("[G•] screen changed!")
       
       // Assumes permission was granted for the demo. Future work is to send these images to the cloud.
       PHPhotoLibrary.shared().performChanges({
         let options = PHAssetResourceCreationOptions()
         let creationRequest = PHAssetCreationRequest.forAsset()
-        if let jpegScreen = UIImage(ciImage: currentScreen).jpegData(compressionQuality: 1.0) {
-          creationRequest.addResource(with: .photo, data: jpegScreen, options: options)
-        }
+        creationRequest.addResource(with: .photo, fileURL: currentScreenJpegUrl, options: options)
       }) { success, error in
         if success {
           RecordingStatus.didRecordSample()
@@ -79,8 +89,20 @@ class SampleHandler: RPBroadcastSampleHandler {
         }
       }
     } else {
-      NSLog("[G•] screen stayed the same")
+      os_log("[G•] screen stayed the same")
     }
+  }
+  
+  func recognizeTextHandler(request: VNRequest, error: Error?) {
+    guard let observations = request.results as? [VNRecognizedTextObservation] else {
+      return
+    }
+    let recognizedStrings = observations.compactMap { observation in
+      return observation.topCandidates(1).first?.string
+    }
+    
+    os_log("[G•] Found text: \(recognizedStrings.description, privacy: .public)")
+    
   }
   
   private func getCIImageFrom(_ sampleBuffer: CMSampleBuffer) -> CIImage? {
@@ -92,12 +114,33 @@ class SampleHandler: RPBroadcastSampleHandler {
   }
   
   // Otherwise the extension runs out of memory.
-  func getThumbnailCGImageFrom(_ ciImage: CIImage) -> CGImage? {
+  private func getThumbnailCGImageFrom(_ ciImage: CIImage) -> CGImage? {
     let scaledImage = ciImage.transformed(by: CGAffineTransform(scaleX: 0.25, y: 0.25))
     let cgImage = ciContext.createCGImage(scaledImage, from: scaledImage.extent)
     ciContext.clearCaches()
     return cgImage
   }
   
+  private func saveAsJPEG(_ image: CIImage, name: String) -> URL? {
+    guard let destinationURL = try? FileManager.default.url(for:.documentDirectory,
+                                                            in: .userDomainMask,
+                                                            appropriateFor: nil,
+                                                            create: true)
+      .appendingPathComponent(name),
+          let colorSpace = CGColorSpace(name: CGColorSpace.sRGB)
+    else { return nil }
+    
+    do {
+      let options = [kCGImageDestinationLossyCompressionQuality as CIImageRepresentationOption : 1.0]
+      try ciContext.writeJPEGRepresentation(of: image,
+                                            to: destinationURL,
+                                            colorSpace: colorSpace,
+                                            options: options)
+      return destinationURL
+    } catch {
+      return nil
+    }
+  }
 }
+
 
